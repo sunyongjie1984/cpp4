487f2a83e5aff75761c730f5025fd90bcd5e4da5
yongjies <yongjies@cadence.com>
2019-04-15 16:12:23 +0800

    modify

---
+++ good/2_variables_and_basicTypes/2.3_variables/define_a_variable/define.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main( ) {
+    int a = 10; // An integer
+    int* b = &a; // A pointer to an integer
+    std::cout << *b << std::endl;
+    int** c = &b; // A pointer to a pointer to an integer
+    std::cout << **c << std::endl;
+
+    int d[ 3 ] = { 2, 8, 7 }; // An array of 10 integers
+    std::cout << d[ 0 ] << " " << d[ 1 ] << " " << d[ 2 ] << std::endl;
+    int* e[ 10 ]; // An array of 10 pointers to integers
+    e[ 1 ] = &a;
+    std::cout << *b << std::endl;
+
+    int ( *f )[ 10 ]; // A pointer to an array of 10 integers
+    int ( *g )( int ); // A pointer to a function a that takes an integer argument and returns an integer
+    int ( *h[ 10 ] )( int ); // An array of 10 pointers to functions that take an integer argument and return an integer
+    return 0;
+}
