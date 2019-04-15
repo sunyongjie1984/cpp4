6c942c4030e6b2809773fce798c31957108046f5
yongjies <yongjies@cadence.com>
2019-04-15 16:18:16 +0800

    modify

--- good/2_variables_and_basicTypes/2.3_variables/define_a_variable/define.cpp
+++ good/2_variables_and_basicTypes/2.3_variables/define_a_variable/define.cpp
@@ -12,7 +12,12 @@ int main( ) {
     e[ 1 ] = &a;
     std::cout << *b << std::endl;
 
-    int ( *f )[ 10 ]; // A pointer to an array of 10 integers
+    int d1[ 4 ] = { 2, 8, 7 }; // An array of 10 integers
+    // error
+    //int ( *f )[ 3 ]; // A pointer to an array of 10 integers
+    int ( *f )[ 4 ]; // A pointer to an array of 10 integers
+    f = &d1;
+
     int ( *g )( int ); // A pointer to a function a that takes an integer argument and returns an integer
     int ( *h[ 10 ] )( int ); // An array of 10 pointers to functions that take an integer argument and return an integer
     return 0;
